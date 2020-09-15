7e65d97dc9f23e62f4b20bac7371e6cdc0fe3ae3
TeanaSh <t.shemetun@gmail.com>
2020-09-15 19:37:50 +0300

    change files 1708Task1.py and Home_Work2_Task3.py

---
+++ 1708Task1.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+print('a + b =', (a + b))
+print('a - b =', (a - b))
+print('a * b =', (a * b))
+if b == 0:
+    print("You can't divide on Zero")
+else:
+    print('a / b =', (a / b))
+    
